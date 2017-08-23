af8cfb6fd04634afe0876586eabb87d7f11337e6
unidaddigitalrcnradio <opaez@rcnradio.com.co>
2017-08-23 11:23:20 -0500

    galeria
    galeria

--- src/assets/js/actions.js
+++ src/assets/js/actions.js
@@ -1,12 +1,19 @@
-  $(".ultimair").click(function() {
-      $('html, body').animate({
-          scrollTop: $("#ultima").offset().top - 40
-      }, 1000);
-      $('.header').removeClass('active');
-  });
-// Frases  
+
 $(document).ready(function() {
-      $("#owl-demo").owlCarousel({
+//Menu
+      
+// Frases  
+$("#owl-demo").owlCarousel({
+        navigation : false,
+        pagination:false,
+        slideSpeed : 300,
+        autoPlay: 9000, //Set AutoPlay to 3 seconds
+        paginationSpeed : 400,
+        singleItem : true,
+        transitionStyle : "fade",
+      });    
+// Galeria    
+$("#owl-galeria").owlCarousel({
 
         navigation : false,
         pagination:false,
@@ -15,36 +22,10 @@ $(document).ready(function() {
         paginationSpeed : 400,
         singleItem : true,
         transitionStyle : "fade",
+      });  
     
-      // "singleItem:true" is a shortcut for:
-      // items : 1, 
-      // itemsDesktop : false,
-      // itemsDesktopSmall : false,
-      // itemsTablet: false,
-      // itemsMobile : false
-
-      });
-    });
-
-
-// Galeria
-  $(document).ready(function() {
-      $("#owl-galeria").owlCarousel({
-
-        navigation : false,
-        pagination:false,
-        slideSpeed : 300,
-        autoPlay: 9000, //Set AutoPlay to 3 seconds
-        paginationSpeed : 400,
-        singleItem : true,
-        transitionStyle : "fade",
     
-      // "singleItem:true" is a shortcut for:
-      // items : 1, 
-      // itemsDesktop : false,
-      // itemsDesktopSmall : false,
-      // itemsTablet: false,
-      // itemsMobile : false
+    
+});
 
-      });
-    });
+
