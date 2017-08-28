1f2417ac3cc5ba133ebc3920f90405b52a73e8be
unidaddigitalrcnradio <opaez@rcnradio.com.co>
2017-08-28 16:48:06 -0500

    Front
    Front

---
+++ src/assets/js/actions.js
@@ -0,0 +1,18 @@
+var swiperCert = new Swiper('.certSlider', {
+        slidesPerView: 3    ,
+        paginationClickable: true,
+        spaceBetween: 30,
+        loop:true,
+        autoplay: 4000,
+        breakpoints: {
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            }, 
+            520: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            },
+      
+        }
+    }); 
