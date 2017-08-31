eb0784a9e93ca8af9c2d0fbbbe1f8f59e7b0002d
Ing Leandro Romero <lromero@rcnradio.com.co>
2017-08-31 12:32:14 -0500

    add proxy

--- src/app/noticiasnew/noticiasnew.service.ts
+++ src/app/noticiasnew/noticiasnew.service.ts
@@ -19,7 +19,7 @@ export class NoticiasNewService {
 		var xmlHttp = new XMLHttpRequest();
 		xmlHttp.open( "GET", _url, true ); // false for synchronous request
 
-		 xmlHttp.setRequestHeader("Content-Type","application/json");
+		 //xmlHttp.setRequestHeader("Content-Type","application/json");
 		// xmlHttp.setRequestHeader("X-Requested-With","XMLHttpRequest");
 		// //supported in new browsers...do JSONP based stuff in older browsers...figure out how
 		// xmlHttp.setRequestHeader("Access-Control-Allow-Origin","http://lafm.com.co/");
@@ -29,6 +29,37 @@ export class NoticiasNewService {
 
 		return JSON.parse(xmlHttp.responseText);
 	}
+
+	// getJson(_url){
+	// 	function createCORSRequest(method, url) {
+	// 		var xhr = new XMLHttpRequest();
+	// 		if ("withCredentials" in xhr) {
+		  
+	// 		  // Check if the XMLHttpRequest object has a "withCredentials" property.
+	// 		  // "withCredentials" only exists on XMLHTTPRequest2 objects.
+	// 		  xhr.open(method, url, true);
+		  
+	// 		} else if (typeof XDomainRequest != "undefined") {
+		  
+	// 		  // Otherwise, check if XDomainRequest.
+	// 		  // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+	// 		  xhr = new XDomainRequest();
+	// 		  xhr.open(method, url);
+		  
+	// 		} else {
+		  
+	// 		  // Otherwise, CORS is not supported by the browser.
+	// 		  xhr = null;
+		  
+	// 		}
+	// 		return xhr;
+	// 	  }
+		  
+	// 	  var xhr = createCORSRequest('GET', url);
+	// 	  if (!xhr) {
+	// 		throw new Error('CORS not supported');
+	// 	  }
+	// }
 
 	crearObjNoti(_json){
     	
